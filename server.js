0576c7442614195d6fbbfd16ae758e9b5ffb908f
vigneshmufc <vigneshjune14@gmail.com>
2018-02-26 17:48:42 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,7 @@ var io = require('socket.io')(server);
 var port = process.env.PORT|| 8000;
 
 app.use(express.static(__dirname + '/dist'));
-app.use(function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept-Type');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    next();
-})
+
 io.set('origins', 'https://anytimelibrary.herokuapp.com');
 io.on('connection', (socket) => {
 console.log('new connection made');
